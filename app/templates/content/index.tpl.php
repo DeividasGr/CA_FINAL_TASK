8b1330d9ff01be5a0793dab57df5294c57bc1154
Gruzinas1 <dgruzdzevicius@gmail.com>
2020-12-22 12:24:47 +0200

    media queries for max-width of 400px was created

--- app/templates/content/index.tpl.php
+++ app/templates/content/index.tpl.php
@@ -2,34 +2,40 @@
     <div class="header-img"></div>
     <h1 class="title"><?php print $data['title'] ?></h1>
     <div class="content-container">
-        <div class="content-list">
-            <img src="/media/images/gym.jpg" alt="">
-            <h3>Best Gym</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum leo lectus,
-                malesuada vitae nunc nec, commodo condimentum ipsum.
-                Duis aliquet enim a velit interdum bibendum sit amet quis justo.
-            </p>
+        <div class="flex-box">
+            <div class="content-list">
+                <img src="/media/images/gym.jpg" alt="image about gym">
+                <h3>Best Gym</h3>
+                <p>
+                    A gymnasium, also known as a gym, is a covered location for athletics.
+                    A gym is a place with a number of equipments and machines
+                    used by the people to do exercises.
+                </p>
+            </div>
+            <div class="content-list">
+                <img src="/media/images/spa.jpg" alt="image about spa">
+                <h3>Sauna after training</h3>
+                <p>
+                    A sauna is a small room or building designed as a place to experience dry or wet heat
+                    sessions, or an establishment with one or more of these facilities.
+                    The steam and high heat make the bathers perspire.
+                </p>
+            </div>
+            <div class="content-list">
+                <img src="/media/images/bar.jpg" alt="image about bar">
+                <h3>Replenish your strength</h3>
+                <p>
+                    Cold-pressed juice refers to juice that uses a hydraulic press to extract juice from
+                    fruit and vegetables.
+                </p>
+            </div>
         </div>
-        <div class="content-list">
-            <img src="/media/images/spa.jpg" alt="">
-            <h3>Spa after training</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum leo lectus,
-                malesuada vitae nunc nec, commodo condimentum ipsum.
-                Duis aliquet enim a velit interdum bibendum sit amet quis justo.
-            </p>
-        </div>
-        <div class="content-list">
-            <img src="/media/images/bar.jpg" alt="">
-            <h3>Replenish your strength</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum leo lectus,
-                malesuada vitae nunc nec, commodo condimentum ipsum.
-                Duis aliquet enim a velit interdum bibendum sit amet quis justo.
-            </p>
-        </div>
+
     </div>
 </section>
 <section class="map">
-    <iframe frameborder="0" scrolling="no"  src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=sauletekio%20al%2015+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+    <iframe frameborder="0" scrolling="no"
+            src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=sauletekio%20al%2015+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
 </section>
 <!--<h2 class="heading">--><?php //print $data['heading'] ?><!--</h2>-->
 
