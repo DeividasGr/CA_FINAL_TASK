319345c608283d6097254a96634fde4c73ede050
Gruzinas1 <dgruzdzevicius@gmail.com>
2020-12-22 10:26:44 +0200

    created style for login register and about us pages

--- app/templates/nav.tpl.php
+++ app/templates/nav.tpl.php
@@ -4,15 +4,15 @@
             <ul>
                 <div class="nav-left">
                     <?php foreach ($data['left'] as $title => $link): ?>
-                        <li class="nav__item">
-                            <a href="<?php print $title; ?>" class="nav__link"><?php print $link; ?></a>
+                        <li class="nav-item">
+                            <a href="<?php print $title; ?>" class="nav-link"><?php print $link; ?></a>
                         </li>
                     <?php endforeach; ?>
                 </div>
                 <div class="nav-right">
                     <?php foreach ($data['right'] as $title => $link): ?>
-                        <li class="nav__item">
-                            <a href="<?php print $title; ?>" class="nav__link"><?php print $link; ?></a>
+                        <li class="nav-item">
+                            <a href="<?php print $title; ?>" class="nav-link"><?php print $link; ?></a>
                         </li>
                     <?php endforeach; ?>
                 </div>
